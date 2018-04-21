13f829b427ba70a7908a742f9aab591490a72bab
Hogeyama <gan13027830@gmail.com>
2018-04-21 16:58:03 +0900

    extend the expr with string and some primitives

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,57 @@
 
-import Groningen
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE StrictData                #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# OPTIONS_GHC -Wall                  #-}
 
-fib :: Groningen
-fib = Groningen.fix 1 $ \x -> do
-  y <- Groningen.delay 0 x
-  return $ x + y
+module Main where
 
+import           Groningen
+import           Data.Map   (Map)
+import qualified Data.Map   as M ()
+
+-------------------------------------------------------------------------------
+
+fib :: Groningen 'TyInt
+fib = fix 1 $ \x -> do
+  y <- delay 0 x
+  return (x + y)
+
+fibCode :: (Exp 'TyInt, Map Register Com)
 fibCode = runGroningen fib
+
+fibSym :: [SomeVal]
 fibSym = take 10 $ eval fibCode
 
-shift123 :: Groningen
-shift123 = Groningen.fix 1 $ \x -> do
-  y <- Groningen.delay 3 x
-  z <- Groningen.delay 2 y
+-------------------------------------------------------------------------------
+
+shift123 :: Groningen 'TyInt
+shift123 = fix 1 $ \x -> do
+  y <- delay 3 x
+  z <- delay 2 y
   return z
 
+shift123Code :: (Exp 'TyInt, Map Register Com)
 shift123Code = runGroningen shift123
+
+shift123Sym :: [SomeVal]
 shift123Sym = take 10 $ eval shift123Code
 
+-------------------------------------------------------------------------------
+
+fibShow :: Groningen 'TyString
+fibShow = Show <$> fib
+
+fibWord :: Groningen 'TyString
+fibWord = fix "A" $ \x -> ConcatS x <$> delay "B" x
+
+-------------------------------------------------------------------------------
+
 main :: IO ()
-main = return ()
+main = do
+  print $ take 10 $ eval $ runGroningen fibShow
+  print $ take 05 $ eval $ runGroningen fibWord
 
--- src/Groningen.hs
+++ src/Groningen.hs
@@ -1,96 +1,242 @@
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE FlexibleInstances #-}
 
--- コンストラクタとかは後で隠す
-module Groningen where
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE KindSignatures            #-}
+{-# LANGUAGE LambdaCase                #-}
+{-# LANGUAGE StandaloneDeriving        #-}
+{-# LANGUAGE StrictData                #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# OPTIONS_GHC -Wall                  #-}
 
-import Control.Monad.State
-import Control.Monad.Trans.Writer
+module Groningen
+  (
+  -- Type
+    Type(..)
+  -- Value
+  , Val
+  , IntVal
+  , StringVal
+  , SomeVal
+  -- Expression
+  , VAR
+  , Exp
+  , IntExp
+  , StringExp
+  , SomeExp
+  -- Construct Expression
+  , Groningen
+  , MonadGroningen
+  , GroningenState -- コンストラクタは隠す
+  , fix
+  , delay
+  , runGroningen
+  -- Evaluation
+  , eval
+  ) where
 
--- 変数
-newtype Var = Var Int deriving Show
+import           Control.Arrow          (second)
+import           Control.Lens           (makeLenses, use, view)
+import           Control.Lens.Operators
+import           Control.Monad.State    (State, runState)
+import           Data.Map               (Map, (!))
+import qualified Data.Map               as M
+import           Data.String            (IsString(..))
 
--- とりあえず，データは全て算術式とする
--- 将来的に幽霊型で安全性を担保したい
-data AExp = AInt Integer
-          | AVar Var
-          | APlus AExp AExp
-          | AMinus AExp AExp
-          | ATimes AExp AExp
-          | AAbs AExp
-          | ASign AExp
-          deriving Show
+-------------------------------------------------------------------------------
+-- Data Types
+-------------------------------------------------------------------------------
+
+----------------------------------------
+-- Type
+----------------------------------------
+
+data Type
+  = TyInt
+  | TyString
+
+----------------------------------------
+-- Value
+----------------------------------------
+
+data Val (ty :: Type) where
+  VInt    :: Integer -> Val 'TyInt
+  VString :: String -> Val 'TyString
+deriving instance Eq   (Val ty)
+deriving instance Ord  (Val ty)
+deriving instance Show (Val ty)
+
+type IntVal = Val 'TyInt
+instance Num IntVal where
+  VInt n + VInt m = VInt (n+m)
+  VInt n - VInt m = VInt (n-m)
+  VInt n * VInt m = VInt (n*m)
+  abs (VInt n) = VInt (abs n)
+  signum (VInt n) = VInt (signum n)
+  fromInteger = VInt
+
+type StringVal = Val 'TyString
+instance IsString StringVal where
+  fromString = VString
+
+data SomeVal where
+  SomeVal :: Val ty -> SomeVal
+deriving instance Show SomeVal
+
+----------------------------------------
+-- Expression
+----------------------------------------
+
+type Register = Int
+data VAR (ty :: Type) = VAR Register
+  deriving (Show, Eq, Ord)
+
+data Exp (ty :: Type) where
+  Var    :: VAR ty  -> Exp ty
+  Int    :: Integer -> Exp 'TyInt
+  String :: String -> Exp 'TyString
+  Plus   :: Exp 'TyInt -> Exp 'TyInt -> Exp 'TyInt
+  Minus  :: Exp 'TyInt -> Exp 'TyInt -> Exp 'TyInt
+  Times  :: Exp 'TyInt -> Exp 'TyInt -> Exp 'TyInt
+  Abs    :: Exp 'TyInt -> Exp 'TyInt
+  Sign   :: Exp 'TyInt -> Exp 'TyInt
+
+  -- tekitou primitives
+  Show    :: Exp 'TyInt -> Exp 'TyString
+  LengthS :: Exp 'TyString -> Exp 'TyInt
+  ConcatS :: Exp 'TyString -> Exp 'TyString -> Exp 'TyString
+deriving instance Show (Exp ty)
+
+type IntExp = Exp 'TyInt
+instance Num IntExp where
+  (+) = Plus
+  (-) = Minus
+  (*) = Times
+  abs = Abs
+  signum = Sign
+  fromInteger = Int
+
+type StringExp = Exp 'TyString
+instance IsString StringExp where
+  fromString = String
+
+data SomeExp where
+  SomeExp :: Exp ty -> SomeExp
+deriving instance Show SomeExp
+
+----------------------------------------
+-- Program
+----------------------------------------
 
 -- クロックに同期して動くレジスタ
 -- これの集まりをプログラムとしてみなす
-data Com = Com {
-  -- 初期値
-  ini :: Integer,
-  -- 漸化式
-  recur :: AExp
-} deriving Show
+data Com = Com
+  { ini   :: SomeVal -- ^ 初期値
+  , recur :: SomeExp -- ^ 漸化式
+  } deriving Show
 
--- プログラムを合成するモナド？
--- StateTでプログラムを書き出し，Stateでレジスタを数える
--- リストの末尾にどんどん追加していくのは流石に頭が悪いので，
--- 差分リストとかに置き換えた方が良い
-type Groningen = StateT [Com] (State Int) AExp
+-- プログラムを合成するモナド
+type Groningen ty = MonadGroningen (Exp ty)
+type MonadGroningen = State GroningenState
+data GroningenState = GroningenState
+  { _regCount    :: Register
+  , _regCommands :: Map Register Com
+  }
+makeLenses ''GroningenState
 
--- 自己参照してレジスタを作る
-fix :: Integer -> (AExp -> Groningen) -> Groningen
-fix a f = do
-  -- 現在の状態を覚えておく
-  p <- get
-  x <- lift get
-  -- プログラムを出力させずにとりあえずfを呼び出してみて，
-  -- レジスタがいくら確保されるか確かめる
-  f $ AInt 42
-  y <- lift get
-  -- 巻き戻し
-  put p
-  lift $ put x
-  a' <- f $ AVar $ Var y
-  -- 一回実行してみて，大丈夫そうだった位置にレジスタを置く
-  modify ( ++ [ Com { ini = a, recur = a' } ])
-  lift $ put $ succ y
-  return $ AVar $ Var y
+initialState :: GroningenState
+initialState = GroningenState
+  { _regCount    = 0
+  , _regCommands = M.empty
+  }
+
+-- Operations on State
+----------------------
+
+getRegCount :: MonadGroningen Register
+getRegCount = use regCount
+
+genNewReg :: MonadGroningen Register
+genNewReg = getRegCount <* incRegCount
+
+incRegCount :: MonadGroningen ()
+incRegCount = regCount %= succ
+
+addCom :: Register -> Com -> MonadGroningen ()
+addCom reg com = regCommands %= M.insert reg com
+
+-- Construct Groningen
+----------------------
+
+fix :: Val ty -> (Exp ty -> Groningen ty) -> Groningen ty
+fix i f = do
+  x  <- genNewReg
+  a' <- f $ Var $ VAR x
+  addCom x Com { ini = SomeVal i, recur = SomeExp a' }
+  return $ Var (VAR x)
 
 -- 自己参照せずレジスタを作る
-delay :: Integer -> AExp -> Groningen
+delay :: Val ty -> Exp ty -> Groningen ty
 delay a a' = do
-  x <- lift get
-  modify ( ++ [ Com { ini = a, recur = a' } ])
-  lift $ modify succ
-  return $ AVar $ Var x
+  x <- genNewReg
+  addCom x Com { ini = SomeVal a, recur = SomeExp a' }
+  return $ Var $ VAR x
+
+-------------------------------------------------------------------------------
+-- Evaluation
+-------------------------------------------------------------------------------
 
 -- モナドを走らせてプログラムを作る
-runGroningen :: Groningen -> (AExp, [Com])
-runGroningen m = fst $ flip runState 0 $ runStateT m []
+runGroningen :: Groningen ty -> (Exp ty, Map Register Com)
+runGroningen m = second (view regCommands) $ runState m initialState
 
--- 算術式を評価
-evalAExp :: [Integer] -> AExp -> Integer
-evalAExp env (AInt n) = n
-evalAExp env (AVar (Var x)) = env !! x
-evalAExp env (APlus a1 a2) = evalAExp env a1 + evalAExp env a2
-evalAExp env (AMinus a1 a2) = evalAExp env a1 - evalAExp env a2
-evalAExp env (ATimes a1 a2) = evalAExp env a1 * evalAExp env a2
-evalAExp env (AAbs a) = abs $ evalAExp env a
-evalAExp env (ASign a) = signum $ evalAExp env a
+evalInt :: Map Register SomeVal -> IntExp -> Integer
+evalInt env = \case
+  Int n -> n
+  Var (VAR x) -> case env ! x of
+                    SomeVal (VInt n) -> n
+                    _ -> error "evalInt: ill-typed environment"
+  Plus  a1 a2 -> evalInt env a1 + evalInt env a2
+  Minus a1 a2 -> evalInt env a1 - evalInt env a2
+  Times a1 a2 -> evalInt env a1 * evalInt env a2
+  Abs a       -> abs $ evalInt env a
+  Sign a      -> signum $ evalInt env a
+  LengthS s   -> fromIntegral $ length $ evalString env s
 
--- プログラムを評価
-eval :: (AExp, [Com]) -> [Integer]
-eval (a, p) =
-  map (flip evalAExp a)
-  $ iterate (\r -> map (evalAExp r . recur) p)
-  $ map ini p
+evalString :: Map Register SomeVal -> StringExp -> String
+evalString env = \case
+  String s -> s
+  Show a -> show $ evalInt env a
+  ConcatS s1 s2 -> evalString env s1 ++ evalString env s2
+  Var (VAR x) -> case env ! x of
+                    SomeVal (VString s) -> s
+                    _ -> error "evalInt: ill-typed environment"
 
+evalSomeExp :: Map Register SomeVal -> SomeExp -> SomeVal
+evalSomeExp env (SomeExp e) = case e of
+  Var (VAR x) -> env ! x
+  String{}    -> evalS e
+  Show{}      -> evalS e
+  ConcatS{}   -> evalS e
+  Int{}       -> evalI e
+  Plus{}      -> evalI e
+  Minus{}     -> evalI e
+  Times{}     -> evalI e
+  Abs{}       -> evalI e
+  Sign{}      -> evalI e
+  LengthS{}   -> evalI e
+  where
+    evalS = SomeVal . VString . evalString env
+    evalI = SomeVal . VInt . evalInt env
 
-instance Num AExp where
-  ( + ) = APlus
-  ( - ) = AMinus
-  ( * ) = ATimes
-  abs = AAbs
-  signum = ASign
-  fromInteger = AInt
+evalRegs :: Map Register Com -> [Map Register SomeVal]
+evalRegs p =
+  let initialEnv   = M.map ini p
+      updater      = M.map recur p
+      oneClock env = M.map (evalSomeExp env) updater
+  in iterate oneClock initialEnv
+
+eval :: (Exp ty, Map Register Com) -> [SomeVal]
+eval (a, p) = map (evalSomeExp `flip` SomeExp a) $ evalRegs p
 
